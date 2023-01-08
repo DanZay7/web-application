8d139d387536c2374090bff1a2bae620725e316c
DanZay7 <ddz5@tpu.ru>
2023-01-08 08:03:12 +0300

    Initial commit

---
+++ src/main/java/com/example/shop/services/UserService.java
@@ -0,0 +1,33 @@
+package com.example.shop.services;
+
+import com.example.shop.models.User;
+import com.example.shop.models.enums.Role;
+import com.example.shop.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.security.Principal;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user) {
+        String email = user.getEmail();
+        if (userRepository.findByEmail(email) != null) return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Saving new User with email: {}", email);
+        userRepository.save(user);
+        return true;
+    }
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
+    }
+}
